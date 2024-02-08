1e6b3d3a20326404ea2cca9144e04110e49d295e
Arda Caliskan <acaliska@usc.edu>
2024-02-08 14:13:14 -0800

    added touch detection

--- main.c
+++ main.c
@@ -8,29 +8,27 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 #include "touch_detection.h"
+
+void touch_pwm_init(void);
+void analog_timer_init(void);
 
 int main(void)
 {
-	touch_pwm_init();
+	touch_pwm_init();	// Initializes pwm output (deprecated)
+	analog_timer_init();	// Initializes analog polling
+	sei();			//enables interrupts
 	DDRC |= 1 << 0;		// Set PC0 as output (red LED)
-	DDRB |= 1 << 1;		// Set PB1 as output
+	DDRB |= 1 << 0;		// Set PB1 as output
 	TCCR2B |= (0b001 << CS20);	// No prescalar
-	while (1) {
-		if(touched != 0){	// Flag for touch detection
-			PORTB |= 1 << 0;	// for testing purposes, lights up LED
-			_delay_ms(1000);
-			touched = 0;
+	while (1){
+		if(touched){
+			PORTB |= 1 << 0;	//makes the dips
 		}
 		else{
-			PORTC &= ~(1 << PC0);	// Turn off the LED when no longer touching
-		}
-		PORTC |= 1 << PC0;      // Set PC0 to a 1
-		PORTB |= 1 << 1;
-		_delay_us(40);
-		PORTC &= ~(1 << PC0);   // Set PC0 to a 0
-		PORTB &= ~(1 << 1);
-		_delay_us(1);	
+			PORTB &= ~(1 << 0);	// Turn off the LED when no longer touching
+		}	
 	}
 	return 0;   /* never reached */
 }
--- touch_detection.c
+++ touch_detection.c
@@ -1,12 +1,39 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include "touch_detection.h"
 
 volatile char touched = 0;
+volatile char analogtouch = 0;
 
-void touch_pwm_init(void){  // output is on PB3
+void touch_pwm_init(void){ 	// output is on PB3
 	DDRB |= (1<<3);
-	TCCR2A |= (0b11 << WGM20); // fast pwm mode
-	TCCR2A |= (0b10 << COM2A0); // clear OC2A on compare match
+	TCCR2A |= (0b11 << WGM20);	// Fast PWM mode
+	TCCR2A |= (0b10 << COM2A0);	// clear OC2A on compare match
 	// 001 into CS20 of TCCR2B for no prescale
 	OCR2A = 250;
+}
+
+void analog_timer_init(void){  // timer for polling analog flag
+	// for polling
+	TCCR0A |= (0b10 << WGM00);	// CTC mode
+	TCCR0B |= (0b101 << CS00);	// prescalar 1024
+	TIMSK0 |= (1 << OCIE0A);	// enable interrupts
+	
+	//for generating interrupts
+	ACSR |= (1 << ACIE);	//enabling analong interrupts
+
+}
+
+ISR(ANALOG_COMP_vect){	// Called when not touched
+	analogtouch = 1;	// I'm not being touched! 
+}
+
+ISR(TIMER0_COMPA_vect){	// Have I been touched?
+	if(analogtouch){	// no
+		touched = 0;
+		analogtouch = 0;
+	}
+	else{	// yes
+		touched = 1;	
+	}
 }
