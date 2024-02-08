29f26e617995ccae6ab68237ffd268cdfe34db21
Arda Caliskan <acaliska@usc.edu>
2024-02-08 09:07:43 -0800

    added comments

--- main.c
+++ main.c
@@ -12,13 +12,13 @@ int main(void)
 	DDRC |= 1 << 0;          // Set PORTC bit 0 for output
 	DDRB |= 1 << 1;
 	while (1) {
-		if(flag != 0){
-			PORTB |= 1 << 0;
+		if(flag != 0){	// Flag for touch detection
+			PORTB |= 1 << 0;	// for testing purposes, lights up LED
 			_delay_ms(1000);
 			flag = 0;
 		}
 		else{
-			PORTC &= ~(1 << PC0);	
+			PORTC &= ~(1 << PC0);	// Turn off the LED when no longer touching
 		}
 		PORTC |= 1 << PC0;      // Set PC0 to a 1
 		PORTB |= 1 << 1;
