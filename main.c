d9694e62eee46f2f9f663846bf608ca9ecf82d56
Arda Caliskan <acaliska@usc.edu>
2024-02-08 09:04:42 -0800

    added if statement

--- main.c
+++ main.c
@@ -1,21 +1,7 @@
 /*************************************************************
-*       at328-0.c - Demonstrate simple I/O functions of ATmega328
+* EE459 Project (shock therapy) 
+* Riad Alasadi, Arda Caliskan, Tyler Chen
 *
-*       Program loops turning PC0 on and off as fast as possible.
-*
-* The program should generate code in the loop consisting of
-*   LOOP:   SBI  PORTC,0        (2 cycles)
-*           CBI  PORTC,0        (2 cycles)
-*           RJMP LOOP           (2 cycles)
-*
-* PC0 will be low for 4 / XTAL freq
-* PC0 will be high for 2 / XTAL freq
-* A 9.8304MHz clock gives a loop period of about 600 nanoseconds.
-*
-* Revision History
-* Date     Author      Description
-* 09/14/12 A. Weber    Initial Release
-* 11/18/13 A. Weber    Renamed for ATmega328P
 *************************************************************/
 
 #include <avr/io.h>
@@ -23,9 +9,17 @@
 
 int main(void)
 {
-	DDRC |= 0x01;          // Set PORTC bit 0 for output
+	DDRC |= 1 << 0;          // Set PORTC bit 0 for output
 	DDRB |= 1 << 1;
 	while (1) {
+		if(flag != 0){
+			PORTB |= 1 << 0;
+			_delay_ms(1000);
+			flag = 0;
+		}
+		else{
+			PORTC &= ~(1 << PC0);	
+		}
 		PORTC |= 1 << PC0;      // Set PC0 to a 1
 		PORTB |= 1 << 1;
 		_delay_us(40);
