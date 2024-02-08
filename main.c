d6ef9b8593141889b24ef1de564de9d9813c592a
Arda Caliskan <acaliska@usc.edu>
2024-02-08 09:18:20 -0800

    created touch files

--- main.c
+++ main.c
@@ -2,20 +2,22 @@
 * EE459 Project (shock therapy) 
 * Riad Alasadi, Arda Caliskan, Tyler Chen
 *
+*
 *************************************************************/
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <touch_detection.h>
 
 int main(void)
 {
 	DDRC |= 1 << 0;          // Set PORTC bit 0 for output
 	DDRB |= 1 << 1;
 	while (1) {
-		if(flag != 0){	// Flag for touch detection
+		if(touched != 0){	// Flag for touch detection
 			PORTB |= 1 << 0;	// for testing purposes, lights up LED
 			_delay_ms(1000);
-			flag = 0;
+			touched = 0;
 		}
 		else{
 			PORTC &= ~(1 << PC0);	// Turn off the LED when no longer touching
---
+++ touch_detection.h
@@ -0,0 +1,1 @@
+extern volatile bool touched; //flag for detecting if handle is touched
